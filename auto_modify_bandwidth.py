4efbfcb4faaae739a9155daf94ece32436e3ae49
bright <lious68@163.com>
2018-11-09 21:45:52 +0800

    增加设置每次扩容和缩容的带宽大小

--- auto_modify_bandwidth.py
+++ auto_modify_bandwidth.py
@@ -10,19 +10,17 @@ import json
 arg_length = len(sys.argv)
 ApiClient = UcloudApiClient(base_url, public_key, private_key)
 region = 'cn-south-02'
-highBandwidth = 5  #扩容到的最大带宽
-lowBandwidth = 1   #缩容到的最小带宽
+maxBandwidth = 5  #扩容到的最大带宽
+minBandwidth = 1   #缩容到的最小带宽
+stepBandwidth = 3 #每次扩容/缩容的带宽大小，单位M
 eipIdArray = [] #所有EIPID的存储数组，不需要填写，自动提取
-noAjustEip = ['eip-ci34br','eip-e3vzeb'] #手动填写，不参与调整的EIPid，比如这次不参与调整有2个IP
+noAjustEip = ['eip-ci34br'] #手动填写，不参与调整的EIPid，比如这次不参与调整有2个IP
 
 
 #定义修改带宽的类，并定义一些列的方法。
 class modifyBandwidth(object):
-	def __init__(self,eipid,addTo,reduceTo):
+	def __init__(self,eipid):
 		self.eipid = eipid
-		self.addTo = addTo
-		self.reduceTo = reduceTo
-
 
 	def getBandwidth(self):  #定义获取带宽利用率方法，返回带宽利用率，如，0.313
 		Parameters = {
@@ -32,7 +30,6 @@ class modifyBandwidth(object):
 		}
 		response = ApiClient.get("/", Parameters)
 		return response['EIPSet'][0]['CurBandwidth']
-
 		
 	def getEip(self):  #定义获取EIP信息方法，返回当前带宽大小。
 		Parameters = {
@@ -43,28 +40,26 @@ class modifyBandwidth(object):
 		response = ApiClient.get("/", Parameters)
 		return response['EIPSet'][0]['Bandwidth']
 
-
-	def addBandwidth(self): #定义增加带宽的方法
+	def addBandwidth(self,addTo): #定义增加带宽的方法
 		Parameters = {
 			"Action":"ModifyEIPBandwidth",
 			"Region":region,
 			"EIPId":self.eipid,
-			"Bandwidth":self.addTo
-
+			"Bandwidth":addTo
 		}
 		response = ApiClient.get("/", Parameters)
 		print response
 
-
-	def reduceBandwidth(self):  #定义减少带宽的方法
+	def reduceBandwidth(self,reduceTo):  #定义减少带宽的方法
 		Parameters = {
 			"Action":"ModifyEIPBandwidth",
 			"Region":region,
 			"EIPId":self.eipid,
-			"Bandwidth":self.reduceTo
+			"Bandwidth":reduceTo
 		}
 		response = ApiClient.get("/", Parameters)
 		print response
+
 
 def getEIPId(): #获取EIP所有信息
 		Parameters = {
@@ -72,43 +67,37 @@ def getEIPId(): #获取EIP所有信息
 			"Region":region
 		}
 		response = ApiClient.get("/", Parameters)
-		
 		return response
 
 def listEIP(): #从所有信息里提取EIPid，并存入数组eipIdArray里。
 	eipInfor = getEIPId()
 	number =  eipInfor['TotalCount']
-	
 	for i in range(number):
 		eipIdArray.append(eipInfor['EIPSet'][i]['EIPId'])
 	return eipIdArray
 
-
-
 def adjustBandwidth(eipid): #调整带宽主逻辑
-	#参数
-	targetEip = eipid
-	
-	AutoEIP = modifyBandwidth(targetEip,highBandwidth,lowBandwidth)  #类封装给AutoEIP，并传入参数。
+	AutoEIP = modifyBandwidth(eipid)  #类封装给AutoEIP，并传入参数。
 	utilization = AutoEIP.getBandwidth() #带宽使用率，通过类的方法
 	curBandwidth = AutoEIP.getEip() #当前带宽，通过类的方法
-
-	print utilization,curBandwidth
+	print "This EIP %s utilization is %f,and the bandwidth is %dM" % (eipid,utilization,curBandwidth)
 
 	try:
 		#当带宽利用率超过70%，并且当前带宽还未到目标带宽，则直接增加到目标带宽。
-		if utilization >= 0.7 and curBandwidth != highBandwidth :
-			AutoEIP.addBandwidth()
+		if utilization >= 0.7 and curBandwidth <= maxBandwidth:
+			newBandwidth = curBandwidth + stepBandwidth
+			AutoEIP.addBandwidth(newBandwidth)
 		#当前带宽利用率低于10%，并且当前带宽还未到目的地位带宽，则直接减少到目标低位带宽。
-		elif utilization <= 0.1 and curBandwidth != lowBandwidth :
-			AutoEIP.reduceBandwidth()
+		elif utilization <= 0.1 and curBandwidth > minBandwidth:
+			newBandwidth = curBandwidth - stepBandwidth
+			AutoEIP.reduceBandwidth(newBandwidth)
 		else:
-			print "This the max bandwidth or the min bandwidth ,please ajust"	
+			print "Do nothing,This the max bandwidth or the min bandwidth ,please ajust"	
 	except Exception,e:
 		print Exception,":",e
 
 def main():
-	eipIdList = listEIP()
+	eipIdList = listEIP() #获取所有EIPID
 	ajustEip = list(set(eipIdList).difference(set(noAjustEip)))  #剔除不参与的EIP。
 	for i in ajustEip:
 		adjustBandwidth(i)
